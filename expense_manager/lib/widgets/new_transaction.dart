3d865f3cc3ce6a19e25a764a4afaf2f5df29ec6b
Play Store <ps494122@gmail.com>
2021-03-26 16:13:43 +0530

    added DatePicker

--- expense_manager/lib/widgets/new_transaction.dart
+++ expense_manager/lib/widgets/new_transaction.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addTx;
   NewTransaction(this.addTx);
@@ -8,12 +10,13 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+  void _submitData() {
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) return;
 
@@ -25,6 +28,19 @@ class _NewTransactionState extends State<NewTransaction> {
     Navigator.of(context).pop();
   }
 
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -32,23 +48,41 @@ class _NewTransactionState extends State<NewTransaction> {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           TextField(
-            controller: titleController,
-            onSubmitted: (_) => submitData(),
+            controller: _titleController,
+            onSubmitted: (_) => _submitData(),
             decoration: InputDecoration(
               labelText: 'Title',
             ),
           ),
           TextField(
-            controller: amountController,
+            controller: _amountController,
             keyboardType: TextInputType.numberWithOptions(decimal: true),
             onSubmitted: (_) =>
-                submitData(), // '_' is because it will give some data and we don't need or use it now, so we just simply store it and ignore.
+                _submitData(), // '_' is because it will give some data and we don't need or use it now, so we just simply store it and ignore.
             decoration: InputDecoration(
               labelText: 'Amount',
             ),
           ),
+          Container(
+            height: 50,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    _selectedDate == null
+                        ? 'No Date Selected'
+                        : DateFormat.yMd().format(_selectedDate),
+                  ),
+                ),
+                TextButton(
+                  child: Text('Choose Date'),
+                  onPressed: _presentDatePicker,
+                )
+              ],
+            ),
+          ),
           TextButton(
-            onPressed: submitData,
+            onPressed: _submitData,
             child: Text(
               'Add Transaction',
               style: TextStyle(
